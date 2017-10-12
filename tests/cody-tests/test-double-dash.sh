7cae941f3470ea27833b89ea57cf880ac09e0bdb
Cody Phrampus <cphrampus810@gmail.com>
2017-10-11 21:33:14 -0500

    Fixed expected output for new cody test

--- tests/cody-tests/test-double-dash.sh
+++ tests/cody-tests/test-double-dash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 output=$($SCRIPT_NAME -t git add -- 1,2)
-expected="git add -- tests/gitdo-tests/1 tests/gitdo-tests/2"
+expected="git add -- tests/cody-tests/1 tests/cody-tests/2"
 
 # trim trialing spaces
 output=${output%% }
