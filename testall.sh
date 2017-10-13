2c110fbff0a9e344fe79afac7eeb95c71909e805
Cody Phrampus <cphrampus810@gmail.com>
2017-10-12 21:03:16 -0500

    Display script name before each batch of tests

--- testall.sh
+++ testall.sh
@@ -15,12 +15,13 @@ init
 
 for i in gitdo,add cody,"git add"
 do
+	echo "Testing $folder"
 	OLDIFS=$IFS
 	IFS=',' read folder command <<< "${i}"
 	IFS=$OLDIFS
 	export SCRIPT_NAME=$folder
 	export COMMAND=$command
-	pwd
+
 	# get the base for tests
 	source ./tests/test_base.sh
 
@@ -28,7 +29,7 @@ do
 	for test in tests/test-*
 	do
 		echo "$test"
-		source ./$test
+		source ./"$test"
 	done
 done
 clean_up
