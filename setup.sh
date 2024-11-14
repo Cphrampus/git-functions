e11a7a6f63a8e04a7724c54ca0fc07bc5090471d
Cody Phrampus <Cphrampus810@gmail.com>
2024-11-14 16:00:01 -0600

    file dump: 2024-11-14 16:00:01

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# todo add flag for verbose to hide output behind
+
 for i in *
 do
-	if [[ "$i" =~ "setup.sh" || "$i" =~ test.* || "$i" =~ "README.md" ]]
+	# this works off of the assumption that the desired scripts do not have a .sh or .bat extension
+	if [[ "$i" =~ .*\.(sh|bat) || "$i" =~ test.* || "$i" =~ "README.md" ]]
 	then
 		continue
 	fi
 	# was $PWD:h, but that is a zsh path modifier
-	ln -s $PWD/$i $(dirname $PWD)/$i
+	(ln -s $PWD/$i $(dirname $PWD)/$i > /dev/null 2>&1 && echo "linked $i") || (
+		# check if $i is a symlink
+		echo "Failed to link $i: $([[ -L $(dirname $PWD)/$i ]] && echo "Symlink already exists" || echo "Unknown error")"
+	)
 done
